ae8b010885c65a17312ce80e8527eb9cdcd67fb8
Manouchehrabdi <mabdi0258@gmail.com>
2025-03-06 12:52:56 +0330

    Bilateral data connection

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 style="color:red">SignUp</h1>
+  <SignupForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignupForm from "./components/SignupForm.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { SignupForm },
+};
 </script>
 
 <style>
